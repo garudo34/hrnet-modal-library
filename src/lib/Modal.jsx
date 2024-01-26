b8ab546416e2a8581d0b99ae7a496b5f9a074ffe
garudo34 <fabien.peyres@free.fr>
2024-01-26 12:51:37 +0100

    rework on Modal closing

--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
+import propTypes from 'prop-types'
+
 import './Modal.css'
 
+/**
+ * Modal component that can be opened and closed by clicking outside
+ * of the modal, the close button or pressing Escape key .
+ *
+ * @category Components
+ * @component
+ * @returns {React.Component} - The modal component.
+ */
 const Modal = ({ children, isOpen, onClose }) => {
+  // Event function that closes the modal component when the Escape key is pressed
+  const escFunction = (e) => {
+    if (e.key === 'Escape') {
+      onClose()
+    }
+  }
+  useEffect(() => {
+    document.addEventListener('keydown', escFunction)
+    return () => {
+      document.removeEventListener('keydown', escFunction)
+    }
+  }, [escFunction])
   return (
-    isOpen && (
-      <div className='modal'>
-        <div className='modal-content'>
-          <div className='modal-body'>
-            <span className='close' onClick={onClose}>
-              &times;
-            </span>
-            <p>{children}</p>
+    <>
+      {isOpen && (
+        <>
+          <div className='modal-background' onClick={onClose}></div>
+          <div className='modal'>
+            <div className='modal-content'>
+              <span className='close' onClick={onClose}>
+                &times;
+              </span>
+              <p>{children}</p>
+            </div>
           </div>
-        </div>
-      </div>
-    )
+        </>
+      )}
+    </>
   )
+}
+
+Modal.prototype = {
+  /** Content of the modal */
+  children: propTypes.object,
+  /** Boolean that defines if the modal is open or not */
+  isOpen: propTypes.bool,
+  /** Function that is called when the modal is closed */
+  onClose: propTypes.func,
+}
+
+Modal.defaultProps = {
+  onClose: () => {
+    return
+  },
 }
 
 export default Modal
